562fc1316affb899bfd0fdf6475ee56081fc228c
Melih Gunal <gunalmel@med.umich.edu>
2025-01-27 23:36:46 -0500

    completed the transformation of cognitive load plot data. Collecting everything in byte array and stream as byte array is built piecemeal. Chrome dev tools shows 2.30-2.66sec response time for team lead cognitive load 091824 data. Not much different than doing everything in memory. More memory efficient but probabl not worth the code complexity?

--- plotly-processor/src/congnitive_load_plot_data.rs
+++ plotly-processor/src/congnitive_load_plot_data.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug)]
-pub struct CognitiveLoadData{
+pub struct CognitiveLoadPlotData {
     pub x: Vec<String>,
     pub y: Vec<Option<f64>>,
     pub mode: String,
--- web/src/main.rs
+++ web/src/main.rs
@@ -3,10 +3,10 @@ use crate::gdrive_provider::google_data_source::GoogleDriveDataSource;
 use crate::gdrive_provider::google_drive_hub_adapter_builder::GoogleDriveHubAdapterBuilder;
 use crate::utils::date::seconds_to_csv_row_time;
 use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder};
-use bytes::Bytes;
+use bytes::{Bytes, BytesMut};
 use config::config::AppConfig;
 use config::resolve_file_path::{resolve_config_file_path, resolve_first_path};
-use futures::stream;
+use futures::{stream, Stream};
 use futures::StreamExt;
 use gdrive_provider::data_source::DataSource;
 use log::debug;
@@ -15,12 +15,13 @@ use mteam_dashboard_plotly_processor::actions_plot_data::ActionsPlotData;
 use mteam_dashboard_plotly_processor::actions_plot_data_transformers::to_plotly_data;
 use mteam_dashboard_plotly_processor::config::init::init_plot_config;
 use mteam_dashboard_plotly_processor::config::plotly_mappings::PlotlyConfig;
-use mteam_dashboard_plotly_processor::congnitive_load_plot_data::CognitiveLoadData;
-use serde_json::{Deserializer, Value};
+use serde_json::{json, to_string, Deserializer, Value};
 use std::error::Error;
 use std::io::BufReader;
 use std::sync::Arc;
 use std::{env, io};
+use std::pin::Pin;
+use async_stream::stream;
 
 mod gdrive_provider;
 mod utils;
@@ -30,14 +31,14 @@ mod config;
 async fn data_sources(context: web::Data<AppContext>) -> impl Responder {
     match context.datasource_provider.get_main_folder_list().await {
         Ok(folders) => HttpResponse::Ok().json(folders),
-        Err(e) => HttpResponse::NotFound().json(serde_json::json!({"Data sources not found": format!("Failed to get data sources: {:#?}", e)}))
+        Err(e) => HttpResponse::NotFound().json(json!({"Data sources not found": format!("Failed to get data sources: {:#?}", e)}))
     }
 }
 async fn plot_sources(path: web::Path<(String,String)>, context: web::Data<AppContext>) -> impl Responder {
     let (data_source_id, plot_name) = path.into_inner();
     match context.datasource_provider.fetch_json_file_map(data_source_id.as_str(), plot_name.as_str()).await {
         Ok(file_name_map) => HttpResponse::Ok().json(file_name_map),
-        Err(e) => HttpResponse::NotFound().json(serde_json::json!({"Not found": format!("Failed to get file list for the selected data source and plot name: {:#?}", e)}))
+        Err(e) => HttpResponse::NotFound().json(json!({"Not found": format!("Failed to get file list for the selected data source and plot name: {:#?}", e)}))
     }
 }
 async fn test_actions(id: web::Path<String>, context: web::Data<AppContext>) -> impl Responder {
@@ -48,7 +49,7 @@ async fn test_actions(id: web::Path<String>, context: web::Data<AppContext>) -> 
     let json_stream = stream::iter(actions_iterator)
         .filter_map(|result| async move {
             match result {
-                Ok(item) => serde_json::to_string(&item).ok(), // Serialize to JSON
+                Ok(item) => to_string(&item).ok(), // Serialize to JSON
                 Err(err) => {
                     eprintln!("Error processing item: {}", err);
                     None // Skip errored items
@@ -85,18 +86,18 @@ async fn actions(id: web::Path<String>, context: web::Data<AppContext>) -> impl 
     let actions_plot_data: ActionsPlotData = to_plotly_data(context.plotly_config, actions_iterator);
 
     // Serialize the data
-    match serde_json::to_string(&actions_plot_data) {
+    match to_string(&actions_plot_data) {
         Ok(json) => HttpResponse::Ok()
             .content_type("application/json")
             .body(json),
         Err(_) => HttpResponse::InternalServerError().body("Failed to serialize result"),
     }
 }
-async fn cognitive_load(id: web::Path<String>, context: web::Data<AppContext>) -> impl Responder{
-    process_json_file(id.to_string(), context.datasource_provider.clone()).await
-        .map(|data| HttpResponse::Ok().json(data))
-        .unwrap_or_else(|e| HttpResponse::NotFound().json(serde_json::json!({"Not found": format!("Failed to get cognitive load data: {:#?}", e)})))
-}
+// async fn cognitive_load(id: web::Path<String>, context: web::Data<AppContext>) -> impl Responder{
+//     process_json_file(id.to_string(), context.datasource_provider.clone()).await
+//         .map(|data| HttpResponse::Ok().json(data))
+//         .unwrap_or_else(|e| HttpResponse::NotFound().json(serde_json::json!({"Not found": format!("Failed to get cognitive load data: {:#?}", e)})))
+// }
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -167,16 +168,13 @@ fn get_app_config() -> Result<AppConfig, Box<dyn Error>> {
     Ok(config)
 }
 
-async fn process_json_file(file_id: String, data_source: Arc<GoogleDriveDataSource>) -> Result<CognitiveLoadData, String> {
-    let reader = data_source.fetch_json_reader(file_id.clone()).await?;
+async fn process_json_file(file_id: &str, data_source: Arc<GoogleDriveDataSource>) -> Result<impl Iterator<Item=(String, Option<f64>)>, String> {
+    let reader = data_source.fetch_json_reader(file_id.to_owned()).await.map_err(|e| e.to_string())?;
     debug!("Processing JSON file: {}", file_id);
 
     let buf_reader = BufReader::new(reader);
-
-    // Parse the root array lazily using the Deserializer
     let mut stream = Deserializer::from_reader(buf_reader).into_iter::<Value>();
 
-    // Ensure we are working with a single root array
     let root_array = match stream.next() {
         Some(Ok(Value::Array(root))) => root,
         Some(Ok(_)) => return Err("JSON root is not an array".to_string()),
@@ -184,35 +182,67 @@ async fn process_json_file(file_id: String, data_source: Arc<GoogleDriveDataSour
         None => return Err("JSON is empty".to_string()),
     };
 
-    let mut result = CognitiveLoadData {
-        x: Vec::new(),
-        y: Vec::new(),
-        mode: "lines".to_string(),
-        series_type: "scatter".to_string()
-    };
-
     let mut first_timestamp: Option<f64> = None;
 
-    // Process each inner array in the root array
-    for item in root_array {
+    Ok(root_array.into_iter().filter_map(move |item| {
         if let Value::Array(arr) = item {
-            if let (Some(x), y) = (arr.get(0).and_then(Value::as_f64), 
-                                   arr.get(1).and_then(Value::as_f64)) {
-               
+            if let (Some(x), y) = (arr.get(0).and_then(Value::as_f64), arr.get(1).and_then(Value::as_f64)) {
                 if first_timestamp.is_none() {
-                    first_timestamp = Some(x); // Record the first timestamp
+                    first_timestamp = Some(x);
                 }
 
                 if let Some(start_time) = first_timestamp {
                     let elapsed_seconds = (x - start_time) as u32;
                     let csv_row_time = seconds_to_csv_row_time(elapsed_seconds);
-
-                    result.x.push(csv_row_time.date_string);
-                    result.y.push(y);
+                    return Some((csv_row_time.date_string, y));
                 }
             }
         }
-    }
-
-    Ok(result)
+        None
+    }))
 }
+
+async fn cognitive_load(id: web::Path<String>, context: web::Data<AppContext>) -> impl Responder {
+    let stream = stream! {
+        match process_json_file(id.as_str(), context.datasource_provider.clone()).await {
+            Ok(iterator) => {
+                // Start the JSON object
+                yield Ok(Bytes::from("{\"x\":[".to_string()));
+                let mut y_bytes = BytesMut::new();
+                y_bytes.extend_from_slice(b"],\"y\":[");
+                
+                let mut first = true;
+                
+                for (x, y) in iterator {
+                    if !first {
+                        y_bytes.extend_from_slice(b",");
+                        yield Ok(Bytes::from(",".to_string()));
+                    }
+                    first = false;
+                    let x_point = json!(x);
+                    let y_point = json!(y);
+                    y_bytes.extend_from_slice(to_string(&y_point).unwrap().as_bytes());
+                    yield Ok(Bytes::from(to_string(&x_point).unwrap()));
+                }
+
+                yield Ok(y_bytes.freeze());
+
+                // Close the `y` array and add other fields
+                yield Ok(Bytes::from("],\"mode\":\"lines\",\"type\":\"scatter\"}".to_string()));
+            }
+            Err(err) => {
+                yield Err(io::Error::new(io::ErrorKind::Other, err));
+            }
+        }
+    };
+    // Create the streaming response
+
+    // Convert the stream to a Pin<Box<dyn Stream>>
+    let body: Pin<Box<dyn Stream<Item = Result<Bytes, io::Error>> + Send>> = Box::pin(stream);
+
+    // Return an HTTP response
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .streaming(body)
+}
+
